feed34a933f9afedff8b2fbe58d57f6eacac300e
m1xture <misha.ivanov.1050@gmail.com>
2024-07-25 23:21:44 +0300

    try

---
+++ src/components/Difficulty/Difficulty.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+const DifficultyBlock = styled.div`
+border-radius: 10px;
+margin-top: 15px;
+bacground-color: teal;
+`
+
+const DifficultyList = styled.ul`
+display: flex;
+
+`
+
+const DifficultyItem = styled.li`
+
+`
+
+export const Difficulty = ({difficulty}) => {
+    return ()
+}
